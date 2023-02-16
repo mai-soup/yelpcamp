2cb7e5fd02084648b89712e5353c540cdac824cd
Maijs Garais <maireadsbooks@gmail.com>
2023-02-16 20:18:55 +0000

    add campground editing, method override module

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const Campground = require("./models/campground");
+
+mongoose.set("strictQuery", false);
 
 mongoose.connect("mongodb://127.0.0.1:27017/yelpcamp", {
     useNewUrlParser: true,
@@ -19,7 +22,8 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -40,11 +44,23 @@ app.get("/campgrounds/:id", async (req, res) => {
     res.render("campgrounds/show", { camp });
 });
 
+app.get("/campgrounds/:id/edit", async (req, res) => {
+    const { id } = req.params;
+    const camp = await Campground.findById(id);
+    res.render("campgrounds/edit", { camp });
+});
+
 app.post("/campgrounds", async (req, res) => {
     const c = new Campground(req.body.campground);
     await c.save();
     console.log(c);
     res.redirect(`/campgrounds/${c._id}`);
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${id}`);
 });
 
 app.listen(3000, () => {
