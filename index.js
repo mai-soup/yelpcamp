5977aa4255761729e3b06223880a428a41aae823
Maijs Garais <maireadsbooks@gmail.com>
2023-02-16 18:07:20 +0000

    display list of dummy camps, details page for each

--- index.js
+++ index.js
@@ -23,10 +23,15 @@ app.get("/", (req, res) => {
     res.render("home");
 })
 
-app.get("/makecampground", async (req, res) => {
-    const camp = new Campground({ title: "Backyard" });
-    await camp.save();
-    res.send(camp);
+app.get("/campgrounds", async (req, res) => {
+    const camps = await Campground.find({});
+    res.render("campgrounds/index", { camps });
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+    const { id } = req.params;
+    const camp = await Campground.findById(id);
+    res.render("campgrounds/show", { camp });
 });
 
 app.listen(3000, () => {
