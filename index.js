643fea2dbd21e62880b34d6524a1bd41cd6157ac
Maijs Garais <maireadsbooks@gmail.com>
2023-02-22 13:41:04 +0000

    add session middleware, flash messages

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
+const Joi = require("joi");
 const Campground = require("./models/campground");
+const { join } = require("path");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 mongoose.set("strictQuery", false);
 
@@ -22,12 +26,49 @@ db.once("open", () => {
 
 const app = express();
 
+// sessions
+app.use(
+    session({
+        secret: 'secret',
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+        },
+    })
+);
+
+// flash msgs
+app.use(flash());
+
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
+const validator = (req, res, next) => {
+    const campValidationSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string().required(),
+            description: Joi.string().required(),
+            location: Joi.string().required()
+        }).required()
+    });
+    const { error } = campValidationSchema.validate(req.body);
+    if (!error) return next();
+    const errorMsg = error.details.map(i => i.message).join(",");
+    throw new ExpressError(400, errorMsg);
+};
+
+app.use((req, res, next) => {
+    res.locals.message = req.flash("success");
+    res.locals.error = req.flash("error");
+    return next();
+});
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -54,23 +95,25 @@ app.get("/campgrounds/:id/edit", catchAsync(async (req, res) => {
     res.render("campgrounds/edit", { camp });
 }));
 
-app.post("/campgrounds", catchAsync(async (req, res) => {
-    if (!req.body.campground) throw new ExpressError(400, "Invalid Campground Data");
+app.post("/campgrounds", validator, catchAsync(async (req, res) => {
     const c = new Campground(req.body.campground);
     await c.save();
     console.log(c);
+    req.flash("success", "Successfully created campground.");
     res.redirect(`/campgrounds/${c._id}`);
 }));
 
-app.put("/campgrounds/:id", catchAsync(async (req, res) => {
+app.put("/campgrounds/:id", validator, catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    req.flash("success", "Successfully updated campground.");
     res.redirect(`/campgrounds/${id}`);
 }));
 
 app.delete("/campgrounds/:id", catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash("success", "Successfully deleted campground.");
     res.redirect("/campgrounds");
 }));
 
@@ -84,6 +127,6 @@ app.use((err, req, res, next) => {
     res.status(status).render("error", { err });
 });
 
-app.listen(3000, () => {
-    console.log("servin on 3000");
+app.listen(3001, () => {
+    console.log("servin on 3001");
 });
