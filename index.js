dcb6f400a8eb50e863eb0f7089145193198b4d94
Maijs Garais <maireadsbooks@gmail.com>
2023-02-25 14:54:57 +0000

    add helmet, download js and css from cdn

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
-const User = require("./models/user")
-const { join } = require("path");
+const User = require("./models/user");
 const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
 
 mongoose.set("strictQuery", false);
 
@@ -58,6 +58,35 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(mongoSanitize());
+
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+
+app.use(
+    helmet({
+        contentSecurityPolicy: {
+            directives: {
+                defaultSrc: [],
+                connectSrc: ["'self'", ...connectSrcUrls],
+                scriptSrc: ["'unsafe-inline'", "'self'"],
+                styleSrc: ["'self'", "'unsafe-inline'"],
+                workerSrc: ["'self'", "blob:"],
+                objectSrc: [],
+                imgSrc: [
+                    "'self'",
+                    "blob:",
+                    "data:",
+                    "https://res.cloudinary.com/dmivfa33d/",
+                ],
+                fontSrc: ["'self'"],
+            },
+        }
+    })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
